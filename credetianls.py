b59c95db9ba9c8d9fd26df27bebf39d799c90b85
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 16:28:35 +0300

    write the user section

--- credetianls.py
+++ credetianls.py
@@ -1,3 +1,17 @@
 # accoun
 class User:
     user_list =[]
+
+
+    def __init__(self, user_name,password):
+        self.user_name = user_name
+        self.password = password
+
+
+ 
+    def save_user(self):
+        
+        '''
+        A method that save an instance of a user in the user list
+        '''
+        User.user_list.append(self)
