c9997e3346699d38710c35c9e15e61a3670ab855
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 18:22:53 +0300

    write the ts file

--- passwordlock.py
+++ passwordlock.py
@@ -8,5 +8,10 @@ def create_user(firstname, lastname, password):
 def save_user(user):
     User.save_user(user)
 
-def 
+def  delete_user(user):
+    user.delete_User()
+
+def authenticate_user(first_name, password):
+     confirm_user = Credential.confirm_user(first_name, password)
+     return confirm_user
 
