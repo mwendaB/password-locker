dcd060239f46552796e7b9fc846547fbac062d76
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 16:43:08 +0300

    write the lock file

--- lock.py
+++ lock.py
@@ -1,10 +1,13 @@
-# cred
-
 import random
 import string
 
 class Credentials:
     Credentials_list = []
+   
     def __init__ (self, account_username, account_password):
         self.account_username = account_username
         self.account_password = account_password
+
+    def delete_logins(self):
+        
+        Credentials.credentials_list.remove(self)
