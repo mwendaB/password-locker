87666429c6da8939557f00af8831620bd2404941
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 19:49:26 +0300

    write the lock file

--- lock.py
+++ lock.py
@@ -6,3 +6,10 @@ from credetianls import Credential
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User('Brian', 'Mwenda', 'mwendaB')
+
+
+
+    def test_init_(self):
+        self.assertEqual(self.new_user.first_name, 'Brian')
+        self.assertEqual(self.new_user.last_name, 'Mwenda')
+        self.assertEqual(self.new_user.password, 'mwendaB')
