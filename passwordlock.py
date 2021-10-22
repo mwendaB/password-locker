46b934875e3a666f7783c5ceae26d0a731fe391d
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 17:03:28 +0300

    write the create profile

--- passwordlock.py
+++ passwordlock.py
@@ -6,7 +6,12 @@ from credetianls import User
 def create_user(user_name,password):
     new_user = User(user_name,password)
     return new_user
+
 def display_user():
     return User.display_user()
+
 def del_user(user):
-    
+    user.delete_user()
+
+def save_User(user):
+    user.save_user()
