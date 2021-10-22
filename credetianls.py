59759b5af756c4f682a94f62902f636cff7e7db2
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 17:57:33 +0300

    writing the py files

--- credetianls.py
+++ credetianls.py
@@ -4,7 +4,6 @@ class Credential:
     credentials_list = []
 
     def __init__(self, user_name, social_media, account_name, password):
-       
         self.user_name = user_name
         self.social_media = social_media
         self.account_name = account_name
@@ -12,3 +11,21 @@ class Credential:
 
     def save_credentials(self):
         Credential.credentials_list.append(self)
+    
+    def generate_password():
+        pwchar = string.printable
+        length = int(input('Enter password length desired: '))
+        generate_password = ''
+        for pwchar in range(length):
+            generate_password += random.choice(pwchar)
+        return generate_password
+
+    
+    @classmethod
+    def confirm_user(cls, first_name, password):
+        
+        active_user = ''
+        for user in User.users_list:
+            if (user.first_name == first_name and user.password == password):
+                active_user = user.first_name
+        return active_user
