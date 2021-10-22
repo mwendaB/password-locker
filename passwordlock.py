125e5244022b29f3094d5fe084edffb6ce194917
mwendaB <brian.mwenda@moringaschool.com>
2021-10-22 17:11:44 +0300

    write the access commands

--- passwordlock.py
+++ passwordlock.py
@@ -23,3 +23,18 @@ def main():
         print('-'*60)
         print('\n')
         print("To create a new user account use 'ca': To login to your account use: 'lg': To delete user use: 'dl' To display user use: 'di' To exit use: 'ex'")
+        short_code =input().lower()
+        print('\n')
+
+
+
+        if short_code == 'ca':
+            print("create username")
+            created_user_name = input()
+            print("Want sytem to generate passowrd? 'y' or 'n'")
+            yes = input()
+            if yes == "y": 
+                pass_code = string.ascii_letters
+                create_password = "".join(random.choice(pass_code) for i in range(10)) 
+                print("pasword Generated!!!") 
+                continue       
