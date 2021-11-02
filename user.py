3e877c6ca0fab0bcbe56de752221bea17e855dc1
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-02 10:05:14 +0300

    write the py file

--- user.py
+++ user.py
@@ -1,13 +1,13 @@
-import pyperclip 
+import pyperclip
 import string
 import random
 
 
 class User:
     user_list = []
-    def __init__(self, first_name, last_name, password):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, username, password):
+       
+        self.username = username
         self.password = password
     
     def save_user(self):
@@ -16,28 +16,24 @@ class User:
     def delete_user(self):
         User.user_list.remove(self)
 
+  
     @classmethod
-    def confirm_user(cls, first_name, password):
-        
-        active_user = ''
-        for user in cls.user_list:
-            if (user.first_name == first_name and user.password == password):
-                active_user = user.first_name
-        return active_user
-
+    def display_user(cls):
+       
+        return cls.user_list
 
 
 class Credential:
     
     
     credentials_list = []
-
-   
-    def __init__ (self, user_name, social_media, account_name, password):
-        self.user_name = user_name
-        self.social_media = social_media
-        self.account_name = account_name
-        self.password = password
+    @classmethod
+    def verify_user(cls,username, password):
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
 
     def save_credentials(self):
         Credential.credentials_list.append(self)
