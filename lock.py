608bc1fb0d0003478547913c20d403aa01208ca2
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-02 11:44:12 +0300

    debuging

--- lock.py
+++ lock.py
@@ -2,75 +2,43 @@ import unittest
 from user import User, Credentials
 
 class TestClass(unittest.TestCase):
-    """
-    It will create a  Test class that defines test cases for the User class.
-    """
     def setUp(self):
-        """
-        It creates a method that runs before each individual user test methods run.
-        """
-        self.new_user = User('Annalis', 'Ann000')
+        self.new_user = User('Brian', 'mwendaB')
 
     def test_init(self):
-        """
-        It creates a test case to check if the object has been initialized correctly
-        """
-        self.assertEqual(self.new_user.username, 'Annalis')
-        self.assertEqual(self.new_user.password, 'Ann000')
+        self.assertEqual(self.new_user.username, 'Brian')
+        self.assertEqual(self.new_user.password, 'mwendaB')
 
     def test_save_user(self):
-        """
-        It createss a test case to test if a new user instance has been saved into the User list
-        """
+     
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
 
 class TestCredentials(unittest.TestCase):
-    """
-    It creates a test class that defines test cases for credentials class
-    """
     def setUp(self):
-        """
-        It creates a method that runs before each individual credential test methods run.
-        """
-        self.new_credential = Credentials('Email', 'Annalis', 'Ann000' )
+        self.new_credential = Credentials('Gmail', 'Brian', 'mwendaB' )
 
     def test_init(self):
-        """
-        It will create a test case to check if a new Credentials instance has been initialized correctly
-        """
-        self.assertEqual(self.new_credential.account, 'Email')
-        self.assertEqual(self.new_credential.userName, 'Annalis')
-        self.assertEqual(self.new_credential.password,'Ann000')
+        self.assertEqual(self.new_credential.account, 'Gmail')
+        self.assertEqual(self.new_credential.userName, 'Brian')
+        self.assertEqual(self.new_credential.password,'mwendaB')
 
     def save_credential_test(self):
-        """
-        It creates a test case to test if the crendential object is saved into the credentials list.
-        """
         self.new_credential.save_details()
         self.assertEqual(len(Credentials.credentials_list),1)
 
     def tearDown(self):
-        '''
-        It creates a method that does clean up after each test case has run.
-        '''
         Credentials.credentials_list = []
 
     def test_save_many_accounts(self):
-        '''
-        It creates a test to check if we can save multiple credentials objects to our credentials list
-        '''
         self.new_credential.save_details()
-        test_credential = Credentials("Twitter","Mickey","Mfh45hfk") 
+        test_credential = Credentials("Facebook","Eugin","!@#$%^&*") 
         test_credential.save_details()
         self.assertEqual(len(Credentials.credentials_list),2)
 
     def test_delete_credential(self):
-        """
-        It creates a test method to test if we can remove an account credentials from our credentials_list
-        """
         self.new_credential.save_details()
-        test_credential = Credentials("Twitter","Mickey","Mfh45hfk")
+        test_credential = Credentials()
         test_credential.save_details()
 
         self.new_credential.delete_credentials()
